1c7cd83a1fdb618a20a1af6c6453c4365a7f673c
Andrei Ivanchenko <ivanchenko@tochka.com>
2020-05-10 22:05:26 +0500

    fix output formatting

--- pre_commit_hooks/util.py
+++ pre_commit_hooks/util.py
@@ -4,17 +4,18 @@ from sbt_client import SbtMessageLevel, ExecutionResult
 
 def print_execution_result(result: ExecutionResult) -> None:
     for level in (SbtMessageLevel.INFO, SbtMessageLevel.WARNING, SbtMessageLevel.ERROR):
+        level_str = _print_level(level)
         messages = result[level]
         if messages:
             for message in messages:
-                print(_print_level(level) + message)
+                print(f"{level_str} {message}")
 
 
 def _print_level(level: SbtMessageLevel) -> str:
     if level is SbtMessageLevel.ERROR:
-        return Fore.RED + "[ ERROR ]" + Style.RESET_ALL
+        return Fore.RED + "[ERROR]" + Style.RESET_ALL
     if level is SbtMessageLevel.WARNING:
-        return Fore.YELLOW + "[ WARNING ]" + Style.RESET_ALL
+        return Fore.YELLOW + "[WARNING]" + Style.RESET_ALL
     if level is SbtMessageLevel.INFO:
-        return Fore.GREEN + "[ INFO ]" + Style.RESET_ALL
-    return Style.DIM + "[ DEBUG ]" + Style.RESET_ALL
+        return Fore.GREEN + "[INFO]" + Style.RESET_ALL
+    return Style.DIM + "[DEBUG]" + Style.RESET_ALL
