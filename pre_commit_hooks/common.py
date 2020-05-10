46a907d6c3b0d6b60e296a70efeddd6a0f589b69
Andrei Ivanchenko <ivanchenko@tochka.com>
2020-05-11 00:47:41 +0500

    debug logging

--- pre_commit_hooks/common.py
+++ pre_commit_hooks/common.py
@@ -1,7 +1,9 @@
 import os
+import logging
 from sbt_client import SbtClient
 
 
 def create_client() -> SbtClient:
+    logging.basicConfig(level=logging.DEBUG, filename="sbt-client.log")
     working_directory = os.getcwd()
     return SbtClient(working_directory)
