dcd1f54b1d693dbe5fc21d92ecf4c254d4ae7e9f
Andrei Ivanchenko <ivanchenko@tochka.com>
2020-05-10 17:50:55 +0500

    changes in scalafmt

--- pre_commit_hooks/scalafmt.py
+++ pre_commit_hooks/scalafmt.py
@@ -1,14 +1,22 @@
 import os
+import sys
 import asyncio
 import logging
-from sbt_client import SbtClient
+from sbt_client import SbtClient, SbtError
 
 
 async def _main() -> int:
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG, filename="sbt-client.log")
     working_directory = os.getcwd()
     client = SbtClient(working_directory)
-    await client.execute("clean; scalafmt; scalafmtCheck;")
+    await client.connect()
+    try:
+        await client.execute("clean")
+        await client.execute("scalafmtCheck")
+        await client.execute("scalafmt")
+    except SbtError as error:
+        print(error, file=sys.stderr)
+        return 0
     return 1
 
 
