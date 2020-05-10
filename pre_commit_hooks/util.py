c804ffb67d92dcfb92dda89fba0d6eacd149b81f
Andrei Ivanchenko <ivanchenko@tochka.com>
2020-05-10 22:00:37 +0500

    colorama

--- pre_commit_hooks/util.py
+++ pre_commit_hooks/util.py
@@ -1,3 +1,4 @@
+from colorama import Fore, Style
 from sbt_client import SbtMessageLevel, ExecutionResult
 
 
@@ -5,4 +6,15 @@ def print_execution_result(result: ExecutionResult) -> None:
     for level in (SbtMessageLevel.INFO, SbtMessageLevel.WARNING, SbtMessageLevel.ERROR):
         messages = result[level]
         if messages:
-            print(*messages)
+            for message in messages:
+                print(_print_level(level) + message)
+
+
+def _print_level(level: SbtMessageLevel) -> str:
+    if level is SbtMessageLevel.ERROR:
+        return Fore.RED + "[ ERROR ]" + Style.RESET_ALL
+    if level is SbtMessageLevel.WARNING:
+        return Fore.YELLOW + "[ WARNING ]" + Style.RESET_ALL
+    if level is SbtMessageLevel.INFO:
+        return Fore.GREEN + "[ INFO ]" + Style.RESET_ALL
+    return Style.DIM + "[ DEBUG ]" + Style.RESET_ALL
