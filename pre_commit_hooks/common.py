b726b781c5f2ff027a5377a9634ace370bdea4cb
Andrei Ivanchenko <ivanchenko@tochka.com>
2020-05-11 15:33:43 +0500

    log level info

--- pre_commit_hooks/common.py
+++ pre_commit_hooks/common.py
@@ -4,23 +4,23 @@ import logging
 from sbt_client import SbtClient
 
 
-PLUGIN_DIR = "./project/plugins.sbt"
+PLUGIN_FILE = "./project/plugins.sbt"
 
 
 def create_client() -> SbtClient:
-    logging.basicConfig(level=logging.DEBUG, filename="sbt-client.log")
+    logging.basicConfig(level=logging.INFO)
     working_directory = os.getcwd()
     return SbtClient(working_directory)
 
 
 def require_plugin(required_plugin: str) -> bool:
-    with open(PLUGIN_DIR, "r") as f:
+    with open(PLUGIN_FILE, "r") as f:
         installed = f.read()
         return _require_plugin(required_plugin, installed)
 
 
 def require_plugins(required_plugins: t.List[str]) -> bool:
-    with open(PLUGIN_DIR, "r") as f:
+    with open(PLUGIN_FILE, "r") as f:
         installed = f.read()
         return all(_require_plugin(plugin, installed) for plugin in required_plugins)
 
@@ -28,7 +28,7 @@ def require_plugins(required_plugins: t.List[str]) -> bool:
 def _require_plugin(required_plugin: str, installed_plugins: str) -> bool:
     if required_plugin not in installed_plugins:
         print(
-            f"Plugin '{required_plugin}' not found in {PLUGIN_DIR}, "
+            f"Plugin '{required_plugin}' not found in {PLUGIN_FILE}, "
             "please make sure it is installed and try again."
         )
         return False
